34354b10b648679b10b6914f263ef8855a207ba6
Đào Long Thịnh <thinhdao202@gmail.com>
2021-01-25 20:25:32 +0700

    first

---
+++ controllers/car.js
@@ -0,0 +1,27 @@
+const Car = require("../models/car");
+
+const postCar = async (req, res) => {
+  const { branchId, licensePlate, seats } = req.body;
+
+  try {
+    const foundCar = await Car.findOne({ licensePlate });
+
+    if (foundCar)
+      return res
+        .status(400)
+        .send({ message: "License plate already existed!!" });
+
+    const newCar = new Car({
+      branch: branchId,
+      licensePlate,
+      seats,
+    });
+
+    const result = await newCar.save();
+    res.send(result);
+  } catch (err) {
+    res.status(500).send({ message: "Something went wrong" });
+  }
+};
+
+module.exports = { postCar };
